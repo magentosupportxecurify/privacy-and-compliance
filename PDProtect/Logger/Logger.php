8d069236bedeada77ebeb9abba0895a8424f0b37
magentosupportxecurify <165155352+magentosupportxecurify@users.noreply.github.com>
2026-06-10 12:40:37 +0530

    Delete PDProtect directory

--- PDProtect/Logger/Logger.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace MiniOrange\PDProtect\Logger;
-
-class Logger extends \Monolog\Logger {}
--- PDProtect/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'MiniOrange_PDProtect',
-    __DIR__
-);
